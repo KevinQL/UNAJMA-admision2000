242b9b38e956d79752c479b2ea696c1b9530a7b8
kev22 <unajmakev@gmail.com>
2021-08-27 04:17:25 -0500

    mod studentfiles images

--- views/m_admin/studentfiles_views.php
+++ views/m_admin/studentfiles_views.php
@@ -9,7 +9,7 @@
         <!-- hcatpcha API -->
         <script src="https://hcaptcha.com/1/api.js" async defer></script>
 
-        <title>ADMISION 2000</title>
+        <title>FILES ADMISION 2000</title>
     </head>
     <body class="consult_views">
 
@@ -41,13 +41,132 @@
 
 <!-- PAGINA CONTENIDO MODULO -->
 
-<?php
+<div class="container mt-3 py-3 bg-light">
+    
+    <div class="mb-5">
+      <label for="exampleFormControlInput1" class="form-label">DNI POSTULANTE</label>
+      <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="DNI">
+      <label for="exampleFormControlInput1" class="form-label">DNI POSTULANTE</label>
+      <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="NOMBRE POSTULANTE">
+    </div>
 
-    var_dump(getNameJs());
-
-?>
 
 
+    <div class="mb-1">
+        <label for="formFile" class="form-label">FOTO ROSTRO</label>
+        <form class="row g-2">
+            <div class="col-auto">
+                <input class="form-control" type="file" id="formFile"
+                    onchange = "readImage(this,'.img_rostro','#base64', 150, 150);"
+                >
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary mb-3">SUBIR</button>
+            </div>
+        </form>
+        <img src="https://i.ibb.co/Tm3hb97/image.png" alt="" 
+            class="img_rostro"
+            width="150px" 
+            height="150px"
+        >
+    </div>
+
+    <div class="mb-1">
+        <label for="formFile" class="form-label">FOTO DNI</label>
+        <form class="row g-2">
+            <div class="col-auto">
+                <input class="form-control" type="file" id="formFile"
+                    onchange = "readImage2(this,'.img_dni','#base64', 400, 320);"
+                >
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary mb-3">SUBIR</button>
+            </div>
+        </form>
+        <img src="https://i.ibb.co/Tm3hb97/image.png" alt="" 
+            class="img_dni"
+            width="350px" 
+            height="200px">
+    </div>
+
+    <div class="mb-1">
+        <label for="formFile" class="form-label">FOTO FIRMA POSTULANTE</label>
+        <form class="row g-2">
+            <div class="col-auto">
+                <input class="form-control" type="file" id="formFile"
+                    onchange = "readImage(this,'.img_firma1','#base64', 200, 150);"
+                >
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary mb-3">SUBIR</button>
+            </div>
+        </form>
+        <img src="https://i.ibb.co/Tm3hb97/image.png" alt="" 
+            class="img_firma1"
+            width="150px"  
+            height="100px">
+    </div>
+    
+    <div class="mb-1">
+        <label for="formFile" class="form-label">FOTO FIRMA APODERADO</label>
+        <form class="row g-2">
+            <div class="col-auto">
+                <input class="form-control" type="file" id="formFile"
+                    onchange = "readImage(this,'.img_firma2','#base64', 200, 150);"
+                >
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary mb-3">SUBIR</button>
+            </div>
+        </form>
+        <img src="https://i.ibb.co/Tm3hb97/image.png" alt="" 
+            class="img_firma2"
+            width="150px"  
+            height="100px">
+    </div>
+
+    <div class="mb-1">
+        <label for="formFile" class="form-label">FOTO VOUCHER</label>
+        <form class="row g-2">
+            <div class="col-auto">
+                <input class="form-control" type="file" id="formFile" 
+                    onchange = "readImage(this,'.img_voucher','#base64', 400, 650);">
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary mb-3">SUBIR</button>
+            </div>
+        </form>
+        <img 
+            src="https://i.ibb.co/Tm3hb97/image.png"
+            class="img_voucher" alt="" 
+            width="300px" 
+            height="400px" 
+                >
+    </div>    
+
+
+        <div class="col-12 col-md-6 col-lg-4 mb-4">
+            <label for="imageImport">Voucher</label>
+            <input id="imageImport" type="file" multiple="false" accept=".jpg, .jpeg, .png" class="form-control" required>
+            
+            <img id="blah" src="https://i.ibb.co/Br8tf3Y/Whats-App-Image-2020-09-26-at-12-50-00-PM.jpg" 
+            alt="Tu imagen" style="width: 100%; height: 200px;" />
+
+        </div>
+
+
+</div>
+
+
+
+
+  <div class="container container-event">
+    <h5>Base de codificación de verificando de voucher</h5>
+    <textarea name="base64" id="base64" rows='8' cols='40' disabled required></textarea>
+    <div id="img_otro">
+      <img src="" alt="Image preview" id="preview_new" style="display:">
+    </div>
+  </div>
 
 
 
@@ -72,6 +191,25 @@
             include_once("./views/_partials/cdns_footer.html");
         ?>
         <!-- SCRIPTS FOR THIS PAGE -->
+        <script>
+
+            function dat(fn){
+                setTimeout(() => {
+                    let res = {"res":22};
+                    fn(res);
+                }, 1200);
+            }
+
+            async function ok1(d){
+                console.log("listoo")
+                console.log(d)
+            }
+            
+
+            dat(ok1);
+
+        </script>
+
         <script src="./views/__js/<?=getNameJs()?>"></script>
 
 
